77f265b3e8e89d6bb6c2bdae9b23c3faf00f3703
riporipo <22fi124@ms.dendai.ac.jp>
2024-11-22 15:43:53 +0900

    kadai

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask,render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    tasks = Todo.query.all()
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/create", methods=["POST"])
+def create():
+    title = request.form.get("title")
+    details = request.form.get("details")
+    new_task = Todo(title=title, details=details)
+
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect("/")
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    delete_task = Todo.query.get(id)
+
+    db.session.delete(delete_task)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/edit/<int:id>', methods=["GET", "POST"])
+def update(id):
+    edit_task = Todo.query.get(id)
+    if request.method == "GET":
+        return render_template("edit.html", task=edit_task)
+    else:
+        edit_task.title = request.form.get("title")
+        edit_task.details = request.form.get("details")
+
+        db.session.commit()
+        return redirect("/")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    details = db.Column(db.String(100))
+
+if __name__ == "__main__":
+    app.run(debug=True)
